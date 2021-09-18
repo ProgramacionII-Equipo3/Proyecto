0af988b5f4f858f85649456fa534a9426f8a7f13
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-09-18 14:02:12 -0300

    Reset ConsoleInterface code

--- src/Library/ClientSide/ConsoleInterface.cs
+++ src/Library/ClientSide/ConsoleInterface.cs
@@ -11,141 +11,87 @@ namespace Library.ClientSide
     public class ConsoleInterface : IClientInterface
     {
         /// <summary>
-        /// The current state stack of the interface.
-        /// It expresses the paths of menus and options the user went through.
-        /// Its main purpose is to help the player remember what it was doing.
+        /// The foreground color of the input fields
+        /// </summary>
+        private const ConsoleColor INPUT_FOREGROUND = ConsoleColor.Red;
+
+        /// <summary>
+        /// This attribute stores the chain of operations the program's state is currently in.
+        /// Its main purpose is to remind the user, in case they forget what they were doing.
         /// </summary>
         private List<string> stateStack = new List<string>();
 
-        private string stateStackStr() =>
-            string.Join(" >> ", this.stateStack);
-
         /// <summary>
-        /// Retrieves data from the console after printing a prompt to it.
+        /// The stack state of the program, prepared to be printed into console.
         /// </summary>
-        /// <returns>The user input</returns>
-        private string getInput()
+        private ConsoleText stateString
         {
-            Console.Write($"> ");
-            return Console.ReadLine();
+            get => ConsoleText.FromStrings((string.Join(" >> ", this.stateStack), ConsoleColor.White, ConsoleColor.Black));
+        }
+        
+        /// <summary>
+        /// Returns input from the user after showing a message.
+        /// Together with GetChar(string), they are the only functions through which the object will interact with the console.
+        /// </summary>
+        /// <param name="prevText">The message to show before asking for input. It can be very long.</param>
+        /// <returns>The input given by the user.</returns>
+        private string GetInput(string prevText)
+        {
+            Console.Clear();
+            stateString.ConsolePrint();
+            Console.WriteLine();
+            Console.Write(prevText);
+            Console.ForegroundColor = INPUT_FOREGROUND;
+            string r =  Console.ReadLine();
+            Console.ResetColor();
+            return r;
         }
 
         /// <summary>
-        /// Retrieves data from the console after printing a placeholder hint to it.
+        /// Returns input from the user after showing a message.
+        /// Together with GetChar(string), they are the only functions through which the object will interact with the console.
         /// </summary>
-        /// <param name="name">The placeholder to print</param>
-        /// <returns>The user input</returns>
-        private string getPlaceheldInput(string name)
+        /// <param name="prevText">The message to show before asking for input. It can be very long.</param>
+        /// <returns>The input given by the user.</returns>
+        private string GetInput(ConsoleText prevText)
         {
-            Console.Write($"{name}: ");
-            return Console.ReadLine();
+            Console.Clear();
+            stateString.ConsolePrint();
+            Console.WriteLine();
+            prevText.ConsolePrint();
+            Console.ForegroundColor = INPUT_FOREGROUND;
+            string r =  Console.ReadLine();
+            Console.ResetColor();
+            return r;
         }
 
         /// <summary>
-        /// Determines the user type referenced by the given user input.
+        /// Returns a character from the user after showing a message.
+        /// Together with GetInput(string), they are the only functions through which the object will interact with the console.
         /// </summary>
-        /// <param name="option">The user input to transform into a user type.</param>
-        /// <returns>The user type (if it's not a valid user type, returns null).</returns>
-        private static UserType? stringToUserType(string option)
+        /// <param name="prevText">The message to show before asking for input. It can be very long.</param>
+        /// <returns>The character given by the user.</returns>
+        private char GetChar(ConsoleText prevText)
         {
-            switch(option.ToUpper())
-            {
-                case "A": return UserType.Admin;
-                case "E": return UserType.Entrepeneur;
-                case "C": return UserType.Company;
-                default : return null;
-            }
-        }
-
-        /// <summary>
-        /// Asks the user if they want to retry an operation after failing before.
-        /// </summary>
-        /// <returns>Whether the user wants to retry</returns>
-        private bool TryAgain()
-        {
-            Console.WriteLine("Press \"q\" to quit...");
-            Console.WriteLine("Press any other key to try again...");
+            Console.Clear();
+            stateString.ConsolePrint();
+            Console.WriteLine();
+            prevText.ConsolePrint();
+            Console.ForegroundColor = INPUT_FOREGROUND;
             char r = (char)Console.Read();
-            return !("qQ\uffff".Contains(r));
+            Console.ResetColor();
+            return r;
         }
 
-        /// <summary>
-        /// Repeatedly attempts to retrieve data specified by the function until it succeeds or the user doesn't want to try again.
-        /// </summary>
-        /// <param name="func">The function to execute to retrieve the data.</param>
-        /// <param name="msg">An error message to print before asking to try again.</param>
-        /// <typeparam name="T">The type of the data to retrieve.</typeparam>
-        /// <returns>The data retrieved by func, if it did.</returns>
-        private T TryUntilValid<T>(Func<T> func, string msg)
+
+        private string GetFormInput(params ConsoleText[] prevText)
         {
-            while(true)
-            {
-                T r = func();
-                if(r is T result) return result;
-                Console.WriteLine();
-                if(msg != null) Console.WriteLine(msg);
-                if(!TryAgain()) return default(T);
-            }
+
         }
 
-        /// <summary>
-        /// Repeatedly attempts to retrieve data specified by the function until it succeeds or the user doesn't want to try again.
-        /// </summary>
-        /// <param name="func">The function to execute to retrieve the data, or the error to print before asking to try again.</param>
-        /// <typeparam name="T">The type of the data to retrieve.</typeparam>
-        /// <returns>The data retrieved by func, if it did.</returns>
-        private T TryUntilValid<T>(Func<(T, string)> func)
+        User IClientInterface.SignIn(IDatabaseConnection conn)
         {
-            while(true)
-            {
-                var (r, msg) = func();
-                if(r is T result) return result;
-                if(msg != null) Console.WriteLine(msg);
-                if(!TryAgain()) return default(T);
-            }
+
         }
-
-        /// <summary>
-        /// Attempts to sign in with a concrete user.
-        /// This is a separate method to facilitate debugging.
-        /// </summary>
-        /// <param name="name">The user's name.</param>
-        /// <param name="password">The user's password.</param>
-        /// <param name="conn">The connection to the database.</param>
-        /// <returns>The resulting user if the operation is successful, null if not.</returns>
-        private (User, string) signIn(string name, string password, IDatabaseConnection conn)
-        {
-            SignInResult response = conn.SignIn(name, password);
-            switch(response)
-            {
-                case SignInResult.OkAdmin:         return (new User(UserType.Admin,       name), null);
-                case SignInResult.OkEntrepeneur:   return (new User(UserType.Entrepeneur, name), null);
-                case SignInResult.OkCompany:       return (new User(UserType.Company,     name), null);
-
-                case SignInResult.NotFound:        return (null, "There isn't a user with the specified name.");
-                case SignInResult.InvalidPassword: return (null, "Incorrect password.");
-
-                default: throw new Exception();
-            }
-        }
-
-        /// <summary>
-        /// Performs the operation to sign in to the program.
-        /// </summary>
-        /// <param name="conn">The connection with which the interface accesses the database.</param>
-        /// <returns>The user, if the operation succeeded.</returns>
-        private (User, string) signIn(IDatabaseConnection conn)
-        {
-            if(
-                   TryUntilValid(() => getPlaceheldInput("Name"), "Invalid answer.")       is string   name
-                && TryUntilValid(() => getPlaceheldInput("Password"), "Invalid password.") is string   password
-            )
-                return signIn(name, password, conn);
-            else
-                return (null, null);
-        }
-
-        User IClientInterface.SignIn(IDatabaseConnection conn) =>
-            TryUntilValid<User>(() => signIn(conn));
     }
 }
---
+++ src/Library/ConsoleText.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Library
+{
+    /// <summary>
+    /// This struct represents a colored string for the console.
+    /// </summary>
+    public struct ConsoleTextElement
+    {
+        /// <summary>
+        /// The text to be printed.
+        /// </summary>
+        public string Text;
+
+        /// <summary>
+        /// The background color of the text.
+        /// </summary>
+        public ConsoleColor? BackgroundColor;
+
+        /// <summary>
+        /// The foreground color of the text.
+        /// </summary>
+        public ConsoleColor? ForegroundColor;
+
+        public ConsoleTextElement(string text, ConsoleColor? backgroundColor, ConsoleColor? foregroundColor)
+        {
+            this.Text = text;
+            this.BackgroundColor = backgroundColor;
+            this.ForegroundColor = foregroundColor;
+        }
+    }
+
+    /// <summary>
+    /// This class represents a text which is meant to be printed into console,
+    /// allowing for several colors, both in background and in foreground.
+    /// </summary>
+    public class ConsoleText
+    {
+        /// <summary>
+        /// The list of strings to print into console.
+        /// </summary>
+        private readonly List<ConsoleTextElement> _strings = new List<ConsoleTextElement>();
+
+        /// <summary>
+        /// A public readonly wrapper of the colored strings.
+        /// </summary>
+        public ReadOnlyCollection<ConsoleTextElement> Strings
+        {
+            get => _strings.AsReadOnly();
+        }
+
+        /// <summary>
+        /// Adds a new colored string to the text.
+        /// </summary>
+        /// <param name="s">The new string</param>
+        /// <param name="backgroundColor"></param>
+        /// <param name="foregroundColor"></param>
+        public void AddString(string s, ConsoleColor? backgroundColor = null, ConsoleColor? foregroundColor = null)
+        {
+            if(string.IsNullOrEmpty(s)) return;
+
+            if(
+                _strings.Count > 0
+             && _strings[^1].BackgroundColor == backgroundColor
+             && _strings[^1].ForegroundColor == foregroundColor
+            )
+            {
+                _strings[^1] = new ConsoleTextElement(_strings[^1].Text + s, backgroundColor, foregroundColor);
+                return;
+            }
+
+            _strings.Add(new ConsoleTextElement(s, backgroundColor, foregroundColor));
+        }
+
+        public void ConsolePrint()
+        {
+            foreach(ConsoleTextElement textEl in _strings)
+            {
+                Console.ResetColor();
+                if(textEl.BackgroundColor is ConsoleColor background)
+                    Console.BackgroundColor = background;
+                if(textEl.ForegroundColor is ConsoleColor foreground)
+                    Console.ForegroundColor = foreground;
+                Console.Write(textEl.Text);
+            }
+            Console.ResetColor();
+        }
+
+        public static ConsoleText FromStrings(params (string, ConsoleColor?, ConsoleColor?)[] data)
+        {
+            ConsoleText r = new ConsoleText();
+            foreach(var (s, back, fore) in data)
+            {
+                r.AddString(s, back, fore);
+            }
+        }
+    }
+}
